18900e261585dd1314701d262e47544e10bb6e1f
Billah104 <mdmasumbillah.official.in@gmail.com>
2023-04-11 20:48:52 +0600

    blog.jsx add and change

--- src/Componets/Blog/Blog.jsx
+++ src/Componets/Blog/Blog.jsx
@@ -1,30 +1,39 @@
 import React from "react";
+import './Blog.css'
 
 const Blog = () => {
   return (
-    <div>
-      {/* context api */}
+    <div className="blog">
+      
       <div>
         <h1>1: When is the Context Api used?</h1>
-        <p>Ans: Context is primarily used when some data needs to be accessible by many components at different nesting levels. Apply it sparingly because it makes component reuse more difficult. If you only want to avoid passing some props through many levels, component composition is often a simpler solution than context. </p>
+        <p>Ans: The Context API in React can be used in various situations where we need to pass data from a parent component to its deeply nested child components. The Context API is particularly useful when:
+
+          Multiple components need to access the same data: If we have multiple components that need to access the same data, we can use the Context API to avoid prop drilling, which can become unwieldy as our component tree grows deeper.
+
+           </p>
       </div>
-      {/* custom  hook */}
+      
       <div>
         <h1>2: What is custom HOOK ?</h1>
-        <p>Ans: When you have component logic that needs to be used by multiple components, we can extract that logic to a custom Hook. </p>
+        <p>Ans: In React, a custom hook is a JavaScript function that starts with the prefix "use"  and follows the rules of the Hooks API. Custom hooks allow we to reuse logic across components by extracting it into a reusable function.
+
+           </p>
       </div>
-      {/* useEffect */}
+      
 
       <div>
         <h1>3: What is useEffect ?</h1>
-        <p>Ans: useEffect is a React Hook that lets you synchronize a component with an external system.</p>
+        <p>Ans: useEffect is a built-in hook in React that allows we to perform side effects in our functional components. Side effects refer to any operation that affects something outside the scope of the current function, such as manipulating the DOM, fetching data from an API, or setting up event listeners.
+
+          </p>
       </div>
-      {/* useMemo */}
+     
       <div>
         <h1>4: What is useMemo ?</h1>
-        <p>Ans: useMemo is a hook that is used in the functional component of React that returns a memoized value.</p>
+        <p>Ans: useMemo is a built-in hook in React that allows you to optimize the performance of your functional components by memoizing the result of expensive calculations. Memoization is a technique that involves caching the result of a function call so that subsequent calls with the same inputs can be returned from the cache instead of re-executing the function.</p>
       </div>
-    
+
     </div>
   );
 };
